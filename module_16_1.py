65349806a20e017d769b10270b2bfc7897fb8304
AndreiVakhrushev <vakhroushev@bk.ru>
2024-12-17 19:36:55 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -1,24 +1,23 @@
 from fastapi import FastAPI
-
 # Создаем экземпляр приложения FastAPI
 app = FastAPI()
 # Создаем маршрут к главной странице - "/".
 # По нему должно выводиться сообщение "Главная страница".
 @app.get("/")
-async def welcome() -> dict:
-    return{"message": "Главная страница"}
+async def welcome() -> str:
+    return(f"Главная страница")
 # Создаем маршрут к странице администратора - "/user/admin".
 # По нему должно выводиться сообщение "Вы вошли как администратор".
 @app.get("/user/admin")
-async def admin() -> dict:
-    return{"message": "Вы вошли как администратор"}
+async def admin() -> str:
+    return(f"Вы вошли как администратор")
 # Создаем маршрут к страницам пользователей используя параметр в пути - "/user/{user_id}".
 # По нему должно выводиться сообщение "Вы вошли как пользователь № <user_id>".
 @app.get("/user/{user_id}")
-async def user_id(user_id:str) -> dict:
-    return{"message": f"Вы вошли как пользователь № {user_id}"}
+async def user_id(user_id:str) -> str:
+    return(f"Вы вошли как пользователь № {user_id}")
 # Создаем маршрут к страницам пользователей передавая данные в адресной строке - "/user".
 # По нему должно выводиться сообщение "Информация о пользователе. Имя: <username>, Возраст: <age>".
 @app.get("/user/{user_name}/{age}")
-async def user_info(user_name:str, age: str) -> dict:
-    return{"message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}
+async def user_info(user_name:str, age: str) -> str:
+    return(f"Информация о пользователе. Имя: {user_name}, Возраст: {age}")
