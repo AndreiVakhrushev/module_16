00cc527c4e2bed06c987d1cf6dbc11e9bb528dbc
AndreiVakhrushev <vakhroushev@bk.ru>
2024-12-23 19:43:02 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -20,16 +20,18 @@ async def create_message(user_name: Annotated[str, Path(min_length=5, max_length
     return f"Пользователь {current_index} зарегистрирован!"
 # put запрос по маршруту '/user/{user_id}/{username}/{age}', который обновляет значение из словаря users
 # под ключом user_id на строку "Имя: {username}, возраст: {age}". И возвращает строку "The user <user_id> is updated"
-@app.put("/user/{user_id}/{user_name}/{age}")
-async def update_message(user_id: Annotated[int, Path(ge=0)], user_name: str = Path(min_length=5, max_length=20,
-                        description="Введите имя", example="Andrei"), age: int = Path(ge=1, le=100,
-                        description="Введите возраст", example=55),) -> str:
-    users[user_id] = f"Имя: {user_name}, возраст: {age}"
-    return f"Информация о пользователе id# {user_id} обновлена!"
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=1000)],
+                      username: Annotated[str, Path(min_length=5, max_length=20,description="Введите имя", example="Andrei")],
+                      age: Annotated[int, Path(ge=1, le=100, description="Введите возраст", example=55)] ):
+    for u_id in users:
+        if int(u_id) == user_id:
+            users[u_id] = f'Имя: {username}, возраст: {age}'
+            return f'Информация о пользователе id# {user_id} обновлена!'
 # delete запрос по маршруту '/user/{user_id}', который удаляет из словаря users по ключу user_id пару
-@app.delete("/user/{user_id}")
-async def delete_user(user_id):
-    if user_id in users:
-        users.pop(user_id)
-        return f"Пользователь с id# {user_id} удален."
-    raise HTTPException(status_code=404, detail="Пользователь не найден.")
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=1000)]):
+    for u_id in users:
+        if int(u_id) == user_id:
+            del users[u_id]
+            return f'Пользователь с id# {user_id} удален.'
